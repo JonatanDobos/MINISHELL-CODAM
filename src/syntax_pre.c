e166682857b45a76aa65e7f2ffd097402ad21f5a
Simon <svan-hoo@student.codam>
2024-08-15 15:25:57 +0200

    cleared up syntax_pre.c

--- src/syntax_pre.c
+++ src/syntax_pre.c
@@ -1,28 +1,6 @@
 #include "../minishell.h"
 
-// static bool	syntax_quotes(const char *line)
-// {
-// 	size_t	i;
-// 	t_uint	s_quote_count;
-// 	t_uint	d_quote_count;
-
-// 	i = 0;
-// 	s_quote_count = 0;
-// 	d_quote_count = 0;
-// 	while (line[i])
-// 	{
-// 		if (line[i] == '\'' && d_quote_count % 2 == 0) // in case "don't hello world" or 'for "whatever reason'
-// 			++s_quote_count;
-// 		if (line[i] == '\"' && s_quote_count % 2 == 0)
-// 			++d_quote_count;
-// 		++i;
-// 	}
-// 	if (s_quote_count % 2 != 0 || d_quote_count % 2 != 0)
-// 		return (false);
-// 	return (true);
-// }
-
-static bool	syntax_quotes(const char *line)
+bool	syntax_pre(const char *line)
 {
 	char	quote;
 	size_t	i;
@@ -40,13 +18,5 @@ static bool	syntax_quotes(const char *line)
 		}
 		++i;
 	}
-	return (true);
-}
-
-bool	syntax_pre(const char *line)
-{
-	if (syntax_quotes(line) == false)
-		return (false);
-	// what else?
 	return (true);
 }
